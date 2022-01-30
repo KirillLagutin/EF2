e8ed8a6096f37eba9784e9c446153c01ccbe6763
Kirill Lagutin <lagutin.k@mail.ru>
2022-01-30 23:51:11 +0300

    Init

---
+++ EF2.TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using EF2.DB_Lib;
+
+namespace EF2.TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var db = new BookStoreDb();
+
+            foreach (var a in db.TabAuthors.ToList())
+            {
+                Console.WriteLine($"#{a.Id}: {a.LastName} {a.FirstName}");
+
+                foreach (var book in a.TabBooks.ToList())
+                {
+                    Console.WriteLine($"\t#{book.Id}: {book.Title} {book.YearOfCreation}");
+                }
+            }
+        }
+    }
+}
